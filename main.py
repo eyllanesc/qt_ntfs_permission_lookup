2417872e17736af85f5e3586a49b4e58a9342071
eyllanesc <e.yllanescucho@gmail.com>
2020-07-30 20:30:10 -0500

    dirty test

--- main.py
+++ main.py
@@ -2,26 +2,30 @@ import os
 from PyQt5 import QtCore
 
 
-CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-filename = os.path.join(CURRENT_DIRECTORY, "test.bin")
+def try_change_permissions():
+    CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+    filename = os.path.join(CURRENT_DIRECTORY, "test.bin")
 
-file = QtCore.QFile(filename)
-if file.open(QtCore.QFile.WriteOnly):
-    file.write(b"helloworld")
-    file.close()
+    file = QtCore.QFile(filename)
+    if file.open(QtCore.QFile.WriteOnly):
+        file.write(b"helloworld")
+        file.close()
 
-file.setPermissions(file.permissions() & QtCore.QFileDevice.ReadUser)
+    file.setPermissions(file.permissions() & QtCore.QFileDevice.ReadUser)
 
-fi = QtCore.QFileInfo(filename)
-print(fi.isWritable())
+    fi = QtCore.QFileInfo(filename)
+    print(fi.isWritable())
+
+    QtCore.QFile.remove(filename)
+
+
+try_change_permissions()
 
 try:
     import qt_ntfs_permission_lookup
 except ImportError:
     pass
 else:
-    print(fi.isWritable())
     qt_ntfs_permission_lookup.enable()
-    print(fi.isWritable())
 
-QtCore.QFile.remove(filename)
+try_change_permissions()
